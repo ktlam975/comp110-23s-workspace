42e221d93f6889c310ecf14623668e9cf5222278
Katie Lam <ktl.katielam@gmail>
2023-01-18 12:50:11 -0500

    Finish Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,4 +1,4 @@
+"""My first program for COMP100."""
+_author_= "730621975"
 print("Hello, world.")
-print("Hello, world, gusgus.")
-"""My first program for COMP110."""
-_author_="730621975"
+print("Hello, world gusgus.")
